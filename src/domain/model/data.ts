b124586d941722cfc45b153655dc9e0934a43d0e
MyVoluntas <privat.gbr@posteo.de>
2019-04-04 19:39:38 +0200

    Update lenses to view and update title of book.

---
+++ src/domain/model/data.ts
@@ -0,0 +1,10 @@
+import {Book, TypeOfBook} from "./Book";
+import {some, none} from "fp-ts/lib/Option";
+import {Person} from "./common";
+
+export const book1 = new Book('12345678hggf', TypeOfBook.Collected, some('1617292249'), some('978-1617292248'),
+                        some(1), some([new Person('Debasish', 'Ghosh')]), none,
+                        some('Manning Publications'), none, some('October 24, 2016'),
+                        some('Functional and Reactive Domain Modeling'),
+                        none, some(320), some('English'), none);
+
